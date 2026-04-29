16490fda9d82ad00856fbd2a98e6a82dfad2efb2
Rahma Atef <roroaatef527@gmail.com>
2026-04-29 23:11:32 +0300

    test main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# نعمل رن للجزء ده الاول علشان نتاكد ان المكتبات شغالة و بعدين نعمله كومنت و نشغل اللى تحت
+
 import pandas as pd
 import seaborn as sns
 import sklearn
@@ -5,5 +7,16 @@ import sklearn
 print("Pandas version:", pd.__version__)
 print("Seaborn version:", sns.__version__)
 print("Scikit-learn version:", sklearn.__version__)
-
 print("\n✅ All libraries are working perfectly!")
+
+#نفك الكومنت من عليه و نعمله رن بعد ما نتاكد ان اللى فوق شغال و نعمل للى فوق كومنت
+"""
+import pandas as pd
+
+# قراءة الداتا
+train_df = pd.read_csv('train_data.csv')
+
+# معرفة هل فيه قيم ناقصة؟
+print("--- Missing Values in each column ---")
+print(train_df.isnull().sum())
+"""
