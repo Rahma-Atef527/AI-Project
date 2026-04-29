752833dc8a43e5842064802bd7ff52b973a2d429
Rahma Atef <roroaatef527@gmail.com>
2026-04-29 21:30:31 +0300

    test_data file and train_data file

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+import sklearn
+
+print("Pandas version:", pd.__version__)
+print("Seaborn version:", sns.__version__)
+print("Scikit-learn version:", sklearn.__version__)
+
+print("\n✅ All libraries are working perfectly!")
